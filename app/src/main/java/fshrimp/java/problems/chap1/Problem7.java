48b94bdee29b2a7ef3bb7d7be372dbcde2b73743
fantasyshrimp <fantasy.shrimp@gmail.com>
2023-01-09 21:21:50 +0900

    solve p7

---
+++ app/src/main/java/fshrimp/java/problems/chap1/Problem7.java
@@ -0,0 +1,17 @@
+package fshrimp.java.problems.chap1;
+
+public class Problem7 {
+
+  public static int convertStringToInt(String str) {
+    return Integer.parseInt(str);
+  }
+  public static long convertStringToLong(String str) {
+    return Long.parseLong(str);
+  }
+  public static float convertStringToFloat(String str) {
+    return Float.parseFloat(str);
+  }
+  public static double convertStringToDouble(String str) {
+    return Double.parseDouble(str);
+  }
+}
